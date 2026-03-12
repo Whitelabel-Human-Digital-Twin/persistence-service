7c75a2a9092a5e68d1a297718e93fc62e8488b93
lm98 <leonardomicelli@gmail.com>
2026-03-12 14:45:34 +0100

    feat: add insert and upsert many operations for hdt and model

--- src/main/kotlin/db/model/ModelService.kt
+++ src/main/kotlin/db/model/ModelService.kt
@@ -3,7 +3,11 @@ package io.github.whdt.db.model
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.MongoDatabase
 import com.mongodb.client.model.Filters.eq
+import com.mongodb.client.model.ReplaceOneModel
 import com.mongodb.client.model.ReplaceOptions
+import com.mongodb.client.model.UpdateOneModel
+import com.mongodb.client.model.UpdateOptions
+import com.mongodb.client.model.Updates
 import io.github.whdt.core.hdt.model.Model
 import io.github.whdt.core.hdt.model.ModelName
 import kotlinx.coroutines.Dispatchers
@@ -26,12 +30,32 @@ class ModelService(private val database: MongoDatabase) {
         doc["_id"].toString()
     }
 
+    suspend fun insertMany(models: List<Model>): Boolean = withContext(Dispatchers.IO) {
+        val docs = models.map { ModelDocument.fromWhdtModel(it) }.map { it.toDocument() }
+        val res = collection.insertMany(docs)
+        res.wasAcknowledged()
+    }
+
     suspend fun upsert(model: Model): Boolean = withContext(Dispatchers.IO) {
         val filter = eq("modelId", model.id.value)
         val options = ReplaceOptions().upsert(true)
         val modelDoc = ModelDocument.fromWhdtModel(model)
         val doc = modelDoc.toDocument()
         val res = collection.replaceOne(filter, doc,options)
+        res.wasAcknowledged()
+    }
+
+    suspend fun upsertMany(models: List<Model>): Boolean = withContext(Dispatchers.IO) {
+        val docs = models.map { ModelDocument.fromWhdtModel(it) }.map { it.toDocument() }
+        val operations = docs.map {
+            val id = it["_id"]
+            ReplaceOneModel(
+                eq("_id", id),
+                it,
+                ReplaceOptions().upsert(true)
+            )
+        }
+        val res = collection.bulkWrite(operations)
         res.wasAcknowledged()
     }
 
