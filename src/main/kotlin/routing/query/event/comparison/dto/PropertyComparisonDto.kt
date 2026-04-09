43c53475a5b237f39e949435cbf384fd71cb150b
lm98 <leonardomicelli@gmail.com>
2026-04-09 17:54:21 +0200

    refactor: add query dto for comparisons

---
+++ src/main/kotlin/routing/query/event/comparison/dto/PropertyComparisonDto.kt
@@ -0,0 +1,22 @@
+package io.github.whdt.routing.query.event.comparison.dto
+
+import io.github.whdt.core.hdt.model.ModelName
+import io.github.whdt.routing.query.event.comparison.ComparisonOperator
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonElement
+import kotlin.time.Instant
+
+@Serializable
+data class PropertyComparisonDto(
+    val propertyName: String,
+    val comparison: ComparisonOperator,
+    val value: JsonElement
+)
+
+@Serializable
+data class PropertiesByComparisonsRequestDto(
+    val comparisons: List<PropertyComparisonDto>,
+    val modelNames: List<ModelName>? = null,
+    val from: Instant? = null,
+    val to: Instant? = null
+)
